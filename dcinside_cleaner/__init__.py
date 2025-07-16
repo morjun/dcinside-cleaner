c4378247ed56696246ad9526e0a850a35d10fa2e
JediMasterJune <anbndn5@gmail.com>
2025-07-16 13:54:40 +0900

    Edit console commands:

--- dcinside_cleaner/__init__.py
+++ dcinside_cleaner/__init__.py
@@ -1,13 +1,13 @@
-from .cli.cleaner_console import Console
-from .gui.cleaner_gui import execute
 
-RUNNING_MODE = 'gui' # 'gui' | 'cli'
+RUNNING_MODE = 'cli' # 'gui' | 'cli'
 
 def main():
     if RUNNING_MODE == 'cli':
+        from .cli.cleaner_console import Console
         Console()
 
     if RUNNING_MODE == 'gui':
+        from .gui.cleaner_gui import execute
         execute()
 
 __all__ = ['main', 'dcinside_cleaner', 'proxy_checker', 'cli', 'gui']
